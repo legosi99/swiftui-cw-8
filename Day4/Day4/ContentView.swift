371cd7100e191e6a1225d1987c87e2cae7289b98
legosi99 <62063512+legosi99@users.noreply.github.com>
2020-12-29 21:18:20 +0300

    button change colors

--- Day4/Day4/ContentView.swift
+++ Day4/Day4/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
     @State var counters = [0,0,0]
     var body: some View {
         VStack(alignment: .trailing) {
-            thekerButton(counter: $counters[0], label: "استغر الله العظيم")
-            thekerButton(counter: $counters[1], label: "سبحان الله وبحمده")
-            thekerButton(counter: $counters[2], label: "الحمدلله")
+            thekerButton(counter: $counters[0], randomColor: .green, label: "استغر الله العظيم")
+            thekerButton(counter: $counters[1], randomColor: .yellow, label: "سبحان الله وبحمده")
+            thekerButton(counter: $counters[2], randomColor: .red, label: "الحمدلله")
         }
     }
 }
@@ -26,6 +26,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct thekerButton: View {
     @Binding var counter: Int
+    @State var randomColor: Color = .red
     let label: String
     var body: some View {
         HStack{
@@ -33,11 +34,14 @@ struct thekerButton: View {
                 .font(.largeTitle)
             Button(action: {
                 counter += 1
+                randomColor = Color(red: .random(in: 0...1)
+                                    , green:.random(in: 0...1)
+                                    , blue: .random(in: 0...1))
             }, label: {
                 Text("\(counter)")
                     .frame(width: 80, height: 80)
                     .accentColor(.white)
-                    .background(Color.green)
+                    .background(randomColor)
                     .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                     .padding()
             })
